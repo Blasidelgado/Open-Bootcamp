01913b0c82a2b873a6894f5b18e0bc996cbab611
Blasidelgado <blasidelgado@gmail.com>
2022-07-25 23:31:57 -0300

    Add exercise 4 solution

---
+++ src/ejer4/ejer4.java
@@ -0,0 +1,44 @@
+package ejer4;
+import java.util.Random;
+
+public class ejer4 {
+    public static void main(String[] args) {
+        int numeroif = new Random().nextInt((20 + 20) + 1) - 20;
+        if (numeroif > 0) {
+            System.out.println(numeroif + " is positive");
+        } else if (numeroif == 0) {
+            System.out.println(numeroif + " is 0");
+        } else {
+            System.out.println(numeroif + " is negative");
+        }
+
+        int numeroWhile = 0;
+
+        while (numeroWhile < 3) {
+            System.out.println(numeroWhile);
+            numeroWhile++;
+        }
+
+        do {
+            System.out.println(numeroWhile + " se ejecuta en mi do.. while una vez");
+        }
+        while (numeroWhile > 50);
+
+        for (int numeroFor = 0; numeroFor <= 3; numeroFor++) {
+            System.out.println(numeroFor);
+        }
+
+        String estacion = "invierno";
+        switch (estacion) {
+            case "verano" : System.out.println("Estamos en verano");
+                break;
+            case "otoño" : System.out.println("Estamos en otoño");
+                break;
+            case "invierno" : System.out.println("Estamos en invierno");
+                break;
+            case "primavera": System.out.println("Estamos en primavera");
+                break;
+            default: System.out.println(estacion + " no es una estación del año");
+        }
+    }
+}
